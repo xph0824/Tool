301a6acc627cfc518a9b0c48e33140e2bbfa0448
SongYanlong <songyanlong@imooc.com>
2018-09-17 18:57:35 +0800

    add function
    add function

--- tools.php
+++ tools.php
@@ -32,6 +32,21 @@ class Tools {
         return $result;
     }
 
+
+    // 二位数组去重
+    public static function array_unique_fb($array2D){
+
+        foreach ($array2D as $v){
+            $v=join(',',$v); //降维,也可以用implode,将一维数组转换为用逗号连接的字符串
+            $temp[]=$v;
+        }
+        $temp=array_unique($temp); //去掉重复的字符串,也就是重复的一维数组
+        foreach ($temp as $k => $v){
+            $temp[$k]=explode(',',$v); //再将拆开的数组重新组装
+        }
+        return $temp;
+    }
+
     /**
      * GET请求某个页面
      *
